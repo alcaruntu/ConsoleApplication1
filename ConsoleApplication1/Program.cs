1e7f81aa1e7d0702e9c03770addef827a40427be
Alex Caruntu <caruntu.alexandru@live.com>
2015-05-13 12:22:18 -0400

    This is a test.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is a comment.
+            var testClass = new TestClass();
+            testClass.Property1 = (string) "123";
+            testClass.Property2 = (string)"1234";
+        }
+    }
+
+    public class TestClass
+    {
+        public string Property1 { get; set; }
+        public string Property2 { get; set; }
+    }
+}
